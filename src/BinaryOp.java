31b6c3deb08f2179717f93a40caefa9fbfe9575b
Shekhar Dangi <arpitt682@gmail.com>
2022-06-26 17:17:16 +0530

    Find the extra character
    You are given two strings s and t.
    String t is generated by random shuffling string s and then add one more letter at a random position.
    Return the letter that was added to t.

--- src/BinaryOp.java
+++ src/BinaryOp.java
@@ -183,4 +183,14 @@ public class BinaryOp {
         }
         return (count == 1) && ((index & 1) == 0);
     }
+    
+    // Find the extra character
+    static char findTheDifference(String s, String t) {
+        int res = 0;
+        for(int i = 0;i < s.length();i++)
+            res ^= s.charAt(i);
+        for(int i = 0;i < t.length();i++)
+            res ^= t.charAt(i);
+        return (char)res;
+    }
 }

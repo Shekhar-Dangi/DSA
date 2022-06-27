b8be0f741caa77693baf48b2f733260413670b68
Shekhar Dangi <arpitt682@gmail.com>
2022-06-27 14:39:07 +0530

    Sieve of Erathosthenes

---
+++ src/Mathematics.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Mathematics {
+    static boolean[] sieveOfEratosthenes(int n) {
+        boolean prime[] = new boolean[n + 1];
+        Arrays.fill(prime, true);
+
+        prime[0] = false;
+        prime[1] = false;
+        for (int i = 2; i * i <= n; i++) {
+            if (prime[i]) {
+                for (int j = i * i; j <= n; j += i)
+                    prime[j] = false;
+            }
+        }
+        return prime;
+    }
+}

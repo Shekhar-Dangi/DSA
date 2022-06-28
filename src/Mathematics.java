a10a375b48c78f381d9aeb44eae855bc11218f9c
Shekhar Dangi <arpitt682@gmail.com>
2022-06-28 14:34:45 +0530

    Formatted Code

--- src/Mathematics.java
+++ src/Mathematics.java
@@ -20,35 +20,32 @@ public class Mathematics {
     static int gcd(int a, int b) {
         return a % b == 0 ? b : gcd(b, a % b);
     }
-    
+
     // Minimum sum of 4 digits
     static int minimumSum(int num) {
         int[] nums = new int[4];
         int i = 0;
-        while(num > 0){
+        while (num > 0) {
             nums[i] = num % 10;
             i++;
             num /= 10;
         }
         Arrays.sort(nums);
-        int res = (nums[2] + 10 * nums[0]) + (nums[3] + 10 * nums[1]);
-        return res;
+        return (nums[2] + 10 * nums[0]) + (nums[3] + 10 * nums[1]);
     }
-    
+
     // Number of good pairs
     static int numIdenticalPairs(int[] nums) {
         Arrays.sort(nums);
         int count = 0;
-        for(int i = 0;i < nums.length;i++){
+        for (int i = 0; i < nums.length; i++) {
             boolean pair = true;
             int k = 1;
-            while(pair){
-                if((i + k < nums.length ) && (nums[i] == nums[i + k])){
+            while (pair) {
+                if ((i + k < nums.length) && (nums[i] == nums[i + k])) {
                     k++;
                     count++;
-                }
-                else
-                    pair = false;
+                } else pair = false;
             }
         }
         return count;
